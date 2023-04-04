43fa63bfb352717662d88e5f3f1072511b33cf3b
Olexandr <ytcahekyttpocto@gmail.com>
2023-04-04 21:08:15 +0300

    Finished

--- script.js
+++ script.js
@@ -29,47 +29,47 @@ article3.addEventListener('click', function(event) {
     article2.classList.remove('active');
 });
 
-// const introHolder = document.querySelector('.intro-holder');
-// const introBlocks = document.querySelectorAll('.intro-block');
-// const introDots = document.querySelectorAll('.intro-dot');
-// const introArrows = document.querySelectorAll('.intro-arrow');
-// let currentSlide = 0;
-//
-// // Add click event listener to dots
-// introDots.forEach((dot, index) => {
-//     dot.addEventListener('click', () => {
-//         goToSlide(index);
-//     });
-// });
-//
-// // Add click event listener to arrows
-// introArrows.forEach(arrow => {
-//     arrow.addEventListener('click', event => {
-//         event.preventDefault();
-//         if (arrow.classList.contains('prev')) {
-//             goToSlide(currentSlide - 1);
-//         } else if (arrow.classList.contains('next')) {
-//             goToSlide(currentSlide + 1);
-//         }
-//     });
-// });
-//
-// // Show slide and update dots
-// function goToSlide(slideIndex) {
-//     if (slideIndex < 0) {
-//         slideIndex = introBlocks.length - 1;
-//     } else if (slideIndex >= introBlocks.length) {
-//         slideIndex = 0;
-//     }
-//     introHolder.style.transform = `translateX(-${slideIndex * 100}%)`;
-//     introDots[currentSlide].classList.remove('active');
-//     introDots[slideIndex].classList.add('active');
-//     currentSlide = slideIndex;
-// }
-//
-// // Auto slide every 5 seconds
-// setInterval(() => {
-//     goToSlide(currentSlide + 1);
-// }, 5000);
+const introHolder = document.querySelector('.intro-holder');
+const introBlocks = document.querySelectorAll('.intro-block');
+const introDots = document.querySelectorAll('.intro-dot');
+const introArrows = document.querySelectorAll('.intro-arrow');
+let currentSlide = 0;
+
+// Add click event listener to dots
+introDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        goToSlide(index);
+    });
+});
+
+// Add click event listener to arrows
+introArrows.forEach(arrow => {
+    arrow.addEventListener('click', event => {
+        event.preventDefault();
+        if (arrow.classList.contains('prev')) {
+            goToSlide(currentSlide - 1);
+        } else if (arrow.classList.contains('next')) {
+            goToSlide(currentSlide + 1);
+        }
+    });
+});
+
+// Show slide and update dots
+function goToSlide(slideIndex) {
+    if (slideIndex < 0) {
+        slideIndex = introBlocks.length - 1;
+    } else if (slideIndex >= introBlocks.length) {
+        slideIndex = 0;
+    }
+    introHolder.style.transform = `translateX(-${slideIndex * 100}%)`;
+    introDots[currentSlide].classList.remove('active');
+    introDots[slideIndex].classList.add('active');
+    currentSlide = slideIndex;
+}
+
+// Auto slide every 5 seconds
+setInterval(() => {
+    goToSlide(currentSlide + 1);
+}, 5000);
 
 
